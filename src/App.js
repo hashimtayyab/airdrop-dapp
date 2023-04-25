1618754a6f02f43b3fb6b54d6db94f4c3915abaf
hashimshah47 <hashimtayyab47@gmail.com>
2023-04-25 12:14:38 +0500

    Updated source

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
 import './App.css';
-import React from 'react';
+import { Container, Row, Col} from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App"
+    <Container className="App" id='myapp'
     style={{
-      height:"auto",
+      height:"100vh",
+      minHeight:"100vh"
     }}>
+      <Row className='topBar'>
+        <Col className='leftNav'>
+          <Row className='hello' id='hello'>
+            <Col>
+             DiversiFi</Col>
+            <Col>Trade</Col>
+            <Col>Swaps</Col>
+            <Col>Yield</Col>
+            <Col>Post</Col>
+            <Col>Send</Col>
+          </Row>
+        </Col>
+        <Col>
+         <button className="claimBtn">Claim DVF</button>
+         <button className="claimBtn">Connect Wallet</button>
+        </Col>
+      </Row>
       
+      <Container id='dialogcontain'>
+      <Row className='dialogBox'>
+      <br />
+          Time to claim
 
-    </div>
+          <span className='textboxx'> Great News- This address is eligible for airdrop! To claim your Token click below to connect to 
+          diversiFi with this address. 
+          </span>
+
+          <button className='mainConnect'>Connect Wallet</button>
+      </Row>
+      </Container>
+    </Container>
   );
 }
 
